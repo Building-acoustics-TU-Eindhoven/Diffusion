c0cfcfac625e5e069e5ea62914983aa555b77d9f
ilariafichera <i.fichera@tue.nl>
2023-03-06 14:35:13 +0100

    Added constant volume of source

--- DiffEq2D.py
+++ DiffEq2D.py
@@ -20,7 +20,7 @@ from scipy import stats
 from acoustics.utils import _is_1d
 from acoustics.signal import bandpass
 from acoustics.bands import (_check_band_type, octave_low, octave_high, third_low, third_high)
-#from FunctionRT import *
+from FunctionRT import *
 from scipy.spatial import KDTree
 
 #General settings
@@ -30,8 +30,8 @@ m_atm = 0 #air absorption coefficient [1/m] from Billon 2008 paper
 pRef = 2 * (10**-5) #Reference pressure
 
 #Spatial discretization
-dx = 0.05 #distance between grid points x direction [m]
-dy = 0.05 #distance between grid points y direction [m]
+dx = 0.1 #distance between grid points x direction [m]
+dy = 0.1 #distance between grid points y direction [m]
 
 #Time discretization
 dt = 0.0001 #distance between grid points on the time discretization [s]
@@ -70,9 +70,9 @@ def abs_term(th,alpha):
         Absx = (c0*alpha)/(2*(2-alpha)) #Modified by Xiang
     return Absx
 
-th = int(input("Enter type Asbortion conditions (option 1,2,3):")) #input 1,2,3 just to understand the type of boundary chosen
-alpha_x = float(input("Enter absorption coefficient:")) #enter absorption coefficient uniform for all the surfaces in the x direction and for one frequency only
-alpha_y = float(input("Enter absorption coefficient:")) #enter absorption coefficient uniform for all the surfaces in the y direction and for one frequency only
+th = 1#int(input("Enter type Asbortion conditions (option 1,2,3):")) #input 1,2,3 just to understand the type of boundary chosen
+alpha_x = 0.99#float(input("Enter absorption coefficient:")) #enter absorption coefficient uniform for all the surfaces in the x direction and for one frequency only
+alpha_y = 0.1#float(input("Enter absorption coefficient:")) #enter absorption coefficient uniform for all the surfaces in the y direction and for one frequency only
 Abs_x = round(abs_term(th,alpha_x),4) #absorption term for x
 Abs_y = round(abs_term(th,alpha_y),4) #absorption term for y
 
@@ -91,11 +91,12 @@ if beta_zero_condition >1:
     print("aa! errors! Check beta condition")
 
 #Set initial condition - Source Info (excitation with Gaussian) 
-Ws=10**-2   #Source point power [Watts] interrupted after 2seconds
-Vs=round(4/3*round(pi,4)*(dx**3),10) #Source volume
-w1 = round(Ws/Vs,4) #energy density of the source [Watts/(m∙(s^3))]
+Ws=10**-2 #Source point power [Watts] interrupted after 2seconds; value taken from Jing 2007; correspondent to a SWL of 100dB
+Vs=0.0001
+#Vs=round(4/3*round(pi,4)*(dx**3),10) #Source volume
+w1 = round(Ws/Vs,4) #power density of the source [Watts/(m^3))]
 
-sourceon_time =  1 #time that the source is on before interrupting [s]
+sourceon_time =  0.5 #time that the source is on before interrupting [s]
 sourceon_steps = ceil(sourceon_time/dt) #time steps at which the source is calculated/considered in the calculation
 s1 = np.multiply(w1,np.ones(sourceon_steps)) #energy density of source number 1 at each time step position
 #####does the source not need to be only at the time 0 to 2seconds and after that there should not be any source term? Yes
@@ -103,8 +104,8 @@ source1 = np.append(s1, np.zeros(recording_steps-sourceon_steps)) #This would be
 
 np.around(s1, 4, s1) #evenly round to the given number of decimals
 
-x_source = 20.5 #position of the source in the x direction [m]
-y_source = 0.75 #position of the source in the x direction [m]
+x_source = 10.2 #position of the source in the x direction [m]
+y_source = 1.2 #position of the source in the x direction [m]
 
 coord_source = [x_source , y_source] #coordinates of the source position in an list
 
@@ -117,8 +118,8 @@ index_source = (np.argwhere((xx == coord_sourceRound0) & (yy == coord_sourceRoun
 rows_s, cols_s = index_source[0], index_source[1] #the row index is the first item in the list; the col index is the second item in the list
 
 #Set initial condition - Receiver Info
-x_rec = 10
-y_rec = 1.5 #lymax/2
+x_rec = 20.2
+y_rec = 2.2 #lymax/2
 #index_receiver = kdt.query([[x_rec,y_rec]])[1] #index of the receiver position
 coord_receiver = [x_rec,y_rec] #coordinates of the receiver position in an list
 index_receiver = (np.argwhere((xx==coord_receiver[0]) & (yy==coord_receiver[1])))[0] #finding the index of the receiver in the meshgrid
@@ -146,7 +147,7 @@ def draw_fig1():
   
 #Function to draw figure2
 def draw_fig2():
-    plt.plot(x, SDL) #plot the figure energy density in decibel in space x
+    plt.plot(x, sdl) #plot the figure energy density in decibel in space x
     plt.grid()
     plt.xlabel("x")
     plt.ylabel("w")
@@ -187,7 +188,7 @@ for steps in range(0, recording_steps):
     w_new[0:Nx,0] = np.divide((4*w_new[0:Nx,1] - w_new[0:Nx,2]),(3+((2*Abs_y*dx)/Dy))) #boundary condition at y=0, any x
     w_new[0:Nx, Ny-1] = np.divide((4*w_new[0:Nx,Ny-2] - w_new[0:Nx,Ny-3]),(3+((2*Abs_y*dx)/Dy))) #boundary condition at at ly=lymax, any x
  
-    SDL = 10*np.log10(abs(w_new)) #sound density level
+    sdl = 10*np.log10(abs(w_new),where=abs(w_new)>0) #sound density level
     #if (steps % 100 == 0): #draw only on certain steps and not all the steps
     #    drawnow(draw_fig)
     #Update w before next step
@@ -201,8 +202,10 @@ for steps in range(0, recording_steps):
 
 #Figure 3: Decay of SPL in the recording_time
 plt.figure(3) 
-SPL = 10*np.log10(((abs(w_rec))*rho*(c0**2))/(pRef**2))
-plt.plot(t,SPL) #plot sound pressure level with Pref = (2e-5)**5
+press_r = ((abs(w_rec))*rho*(c0**2))/(pRef**2)
+#max_press_r = np.max(((abs(w_rec))*rho*(c0**2))/(pRef**2))
+spl = 10*np.log10(press_r) #,where=press_r>0
+plt.plot(t,spl) #plot sound pressure level with Pref = (2e-5)**5
 plt.xlabel("t")
 plt.ylabel("SPL")
 plt.xlim()
@@ -225,7 +228,7 @@ plt.yticks(np.arange(0, -120, -20))
 sch_db[sch_db == -np.inf] = 0 #replace the -inf with zero in the decay
 
 bands = np.array([63,125,250,500,1000,2000,4000]) #array of frequency bands 
-#t60 = t60_impulse(t, sch_db, fspatial, rt='t30') #called function for calculation of t60 
+t60 = t60_decay(t, sch_db, fspatial, rt='t30') #called function for calculation of t60 
 
 
 

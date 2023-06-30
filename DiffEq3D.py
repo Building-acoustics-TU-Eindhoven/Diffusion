9c91e86267f03ba4c450aa8e4ced295c2dc175c2
Fichera <i.fichera@tue.nl>
2023-06-30 14:13:30 +0200

    Trial with Picaut 1997

--- DiffEq3D.py
+++ DiffEq3D.py
@@ -43,7 +43,7 @@ dz = dx #distance between grid points z direction [m]
 
 #Time discretization
 dt = 1/32000 #distance between grid points on the time discretization [s]
-recording_time = 2 #time recorded for the source [s]
+recording_time = 4 #time recorded for the source [s]
 recording_steps = ceil(recording_time/dt) #number of time steps to consider in the calculation
 t = np.arange(0, recording_time, dt) #mesh point in time
 
@@ -52,11 +52,11 @@ fsample = 1/dt #frequency spatial resolution (sampling period)
 
 #Room dimensions
 lxmin = 0 #point x starts at zero [m]
-lxmax = 8 #point x finish at the length of the room in the x direction [m] %Length
+lxmax = 70 #point x finish at the length of the room in the x direction [m] %Length
 lymin = 0 #point y starts at zero [m]
-lymax = 8 #point y finish at the length of the room in the y direction [m] %Width
+lymax = 10 #point y finish at the length of the room in the y direction [m] %Width
 lzmin = 0 #point z starts at zero [m]
-lzmax = 8 #point z finish at the length of the room in the x direction [m] %Height
+lzmax = 10 #point z finish at the length of the room in the x direction [m] %Height
 
 S1,S2 = lxmax*lymax, lxmax*lymax #xy planes
 S3,S4 = lxmax*lzmax, lxmax*lzmax #xz planes
@@ -91,13 +91,13 @@ def abs_term(th,alpha):
         Absx = (c0*alpha)/(2*(2-alpha)) #Modified by Xiang
     return Absx
 
-th = 3 #int(input("Enter type Asbortion conditions (option 1,2,3):")) #input 1,2,3 for different boundary conditions
-alpha_1 = 0.17 #Absorption coefficient for Surface1 - Floor
-alpha_2 = 0.17 #Absorption coefficient for Surface2 - Ceiling
-alpha_3 = 0.17 #Absorption coefficient for Surface3 - Wall Front
-alpha_4 = 0.17 #Absorption coefficient for Surface4 - Wall Back
-alpha_5 = 0.17 #Absorption coefficient for Surface5 - Wall Left
-alpha_6 = 0.17 #Absorption coefficient for Surface6 - Wall Right
+th = 1 #int(input("Enter type Asbortion conditions (option 1,2,3):")) #input 1,2,3 for different boundary conditions
+alpha_1 = 0.1 #Absorption coefficient for Surface1 - Floor
+alpha_2 = 0.1 #Absorption coefficient for Surface2 - Ceiling
+alpha_3 = 0.1 #Absorption coefficient for Surface3 - Wall Front
+alpha_4 = 0.1 #Absorption coefficient for Surface4 - Wall Back
+alpha_5 = 0.1 #Absorption coefficient for Surface5 - Wall Left
+alpha_6 = 0.1 #Absorption coefficient for Surface6 - Wall Right
 
 Abs_1 = abs_term(th,alpha_1) #absorption term for S1
 Abs_2 = abs_term(th,alpha_2) #absorption term for S2
@@ -136,9 +136,9 @@ s1 = np.multiply(w1,np.ones(sourceon_steps)) #energy density of source number 1 
 source1 = np.append(s1, np.zeros(recording_steps-sourceon_steps)) #This would be equal to s1 if and only if recoding_steps = sourceon_steps
 
 #Finding index in meshgrid of the source position
-x_source = 4.0  #position of the source in the x direction [m]
-y_source = 4.2  #position of the source in the y direction [m]
-z_source = 4.2  #position of the source in the z direction [m]
+x_source = 1.0  #position of the source in the x direction [m]
+y_source = 5  #position of the source in the y direction [m]
+z_source = 5  #position of the source in the z direction [m]
 coord_source = [x_source,y_source,z_source] #coordinates of the receiver position in an list
 
 #rows_s = np.argmin(abs(xx[:,0,0] - coord_source[0])) #Find index of grid point with minimum distance from source along x direction
@@ -195,9 +195,9 @@ s[row_upper, col_upper, depth_upper] += source1[1] * weight_row_upper * weight_c
 #############################################################################
 
 #Finding index in meshgrid of the receiver position
-x_rec = 2.0 #position of the receiver in the x direction [m]
-y_rec = 2.0 #position of the receiver in the y direction [m]
-z_rec = 2.0 #position of the receiver in the z direction [m]
+x_rec = 35.0 #position of the receiver in the x direction [m]
+y_rec = 5.0 #position of the receiver in the y direction [m]
+z_rec = 5.0 #position of the receiver in the z direction [m]
 coord_receiver = [x_rec,y_rec,z_rec] #coordinates of the receiver position in an list
 rows_r = np.argmin(abs(xx[:,0,0] - coord_receiver[0])) #Find index of grid point with minimum distance from receiver along x direction
 cols_r = np.argmin(abs(yy[0,:,0] - coord_receiver[1])) #Find index of grid point with minimum distance from receiver along y direction
@@ -405,7 +405,7 @@ plt.show()
 #TRIAL CALCULATION PICAUT 1997
 ##############################################################################
 
-w_inf = w_new[0,:,:].mean() *lzmax/lxmax
+w_inf = w_rec[idx_w_rec]*lzmax/lxmax
 Q = abs(w_rec/w_inf - 1)
 phi = np.log10(Q)
 A_x = np.log10(abs((2*np.cos(np.pi*x/lxmax)))) 
@@ -427,7 +427,6 @@ Diff = -((slope*lxmax**2)/(np.pi**2))
 # Poly-based Approach y = Ax + B
 CoefAlpha = np.polyfit(t[idx_phi:], phi_off, 1) ##calculating the slope and the interception of the line connecting the two points
 slope2 = CoefAlpha[0]
-
 
 Diffusion = (np.subtract(A_x[4], phi)*lxmax**2)/(np.pi**2*t)
 Aminusphi =np.subtract(A_x[4], phi)

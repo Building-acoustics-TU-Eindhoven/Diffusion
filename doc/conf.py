5259d21c3b63d947a72bdb0307c87abc15808e54
Fichera <i.fichera@tue.nl>
2025-09-01 18:32:03 +0200

    Changed configuration file for documentation

--- doc/conf.py
+++ doc/conf.py
@@ -62,3 +62,9 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+myst_enable_extensions = [
+    "amsmath",   # support for $$...$$
+    "dollarmath" # support for $...$ and $$...$$
+]
+

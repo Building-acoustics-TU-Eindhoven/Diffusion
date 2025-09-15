4ac4cb2775e21363bb96f2c0f65d6a116825e46e
Fichera <i.fichera@tue.nl>
2025-09-15 13:37:14 +0200

    Added docstring in Function RT etc...

--- Diffusion_Module/FiniteVolumeMethod/FunctionCentreTime.py
+++ Diffusion_Module/FiniteVolumeMethod/FunctionCentreTime.py
@@ -7,12 +7,24 @@ Created on Thu Mar  9 15:25:01 2023
 
 def centretime(t60, Eq_A, S):
     """
-    Clarity determined from a SPL decay using Barron's revised formula [Vorlander 2008]
-    :param t60: Reverberation time in s
-    :param Eq_A: Equivalent absoption area of the room
-    :param S: total surface area of the rom
-    """
-#    ts = 1000*((t60/13.8) * ((Eq_A/S)+1)) #with direct and reverberant field
+    Calculation of center time Ts from a SPL decay using Barron's revised formula [Vorlander 2008]
+
+    Parameters
+    ----------
+        t60 : float
+            Reverberation time T_{60}
+        Eq_A : array of floats
+            Equivalent absoption area of the room
+        S : float
+            Total surface area of the rom
+
+    Returns
+    -------
+        c80 : float
+            Clarity C_{80}
+    """  
+
+    ts = 1000*((t60/13.8) * ((Eq_A/S)+1)) #with direct and reverberant field
     
-    ts = 1000*((t60/13.8)) #with direct field only
+    #ts = 1000*((t60/13.8)) #with direct field only
     return ts
